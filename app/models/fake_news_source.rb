c6470235e8f0626ac9f1838bdac48cfc471f44a9
Brady Somerville <brady.somerville@gmail.com>
2018-07-29 13:41:23 -0500

    Destroy related fake articles when destroying news source

--- app/models/fake_news_source.rb
+++ app/models/fake_news_source.rb
@@ -1,5 +1,5 @@
 class FakeNewsSource < ApplicationRecord
-  has_many :fake_articles
+  has_many :fake_articles, dependent: :destroy
   validates :query, presence: true, uniqueness: true
   validates_numericality_of :articles_count, only_integer: true, greater_than_or_equal_to: 0
 
